9474e689b4faef4a054cdd177d0fb5eba2db3885
Michael Dobinson <md977102@gmail.com>
2017-01-14 21:57:10 -0500

    UNTESTED: Added register read functionality for codec over I2C

--- src/codec.c
+++ src/codec.c
@@ -11,35 +11,64 @@
 #include "codec.h"
 
 I2C_HandleTypeDef I2cHandle;
-HAL_LockTypeDef a;
 
-void CODEC_i2c2Init( void )
+void CODEC_GPIOInit(void)
 {
 	GPIO_InitTypeDef	GPIO_InitStruct;
 	__GPIOB_CLK_ENABLE();
+
+	// Configure GPIO pins for I2C2
+	GPIO_InitStruct.Pin 		= CODEC_I2C_SCL_PIN | CODEC_I2C_SDA_PIN;
+	GPIO_InitStruct.Mode		= GPIO_MODE_AF_PP;
+	GPIO_InitStruct.Pull		= GPIO_NOPULL; 		// External 4k7 pullups used
+	GPIO_InitStruct.Speed		= GPIO_SPEED_HIGH;
+	GPIO_InitStruct.Alternate	= GPIO_AF4_I2C2;
+	HAL_GPIO_Init(CODEC_I2C_GPIO, &GPIO_InitStruct);
+
+	// Configure GPIO pins for I2S
+	GPIO_InitStruct.Pin 		= CODEC_I2S_WS_PIN | CODEC_I2S_extSD_PIN |
+								  CODEC_I2S_CK_PIN | CODEC_I2S_SD_PIN;
+	GPIO_InitStruct.Mode		= GPIO_MODE_AF_PP;
+	GPIO_InitStruct.Pull		= GPIO_NOPULL; 		// External 4k7 pullups used
+	GPIO_InitStruct.Speed		= GPIO_SPEED_HIGH;
+	GPIO_InitStruct.Alternate	= GPIO_AF5_SPI2;
+	HAL_GPIO_Init(CODEC_I2S_GPIO, &GPIO_InitStruct);
+
+	// Configure I2S MCK
+	GPIO_InitStruct.Pin			= CODEC_I2S_MCK_PIN;
+	HAL_GPIO_Init(CODEC_I2S_MCK_GPIO, &GPIO_InitStruct);
+}
+
+HAL_StatusTypeDef CODEC_i2c2Init(void)
+{
+	HAL_StatusTypeDef	initSuccess;
+
+	// Enable I2C peripheral clocks
 	__HAL_RCC_I2C2_CLK_ENABLE();
 
-	GPIO_InitStruct.Pin 	= GPIO_PIN_10 | GPIO_PIN_11;
-	GPIO_InitStruct.Mode	= GPIO_MODE_AF_PP;
-	GPIO_InitStruct.Pull	= GPIO_PULLUP;
-	GPIO_InitStruct.Speed	= GPIO_SPEED_HIGH;
-	GPIO_InitStruct.Alternate	= GPIO_AF4_I2C2;
-	HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
+	// Configure I2C settings
 	I2cHandle.Instance = I2C2;
 	I2cHandle.Init.AddressingMode 	= I2C_ADDRESSINGMODE_7BIT;
 	I2cHandle.Init.ClockSpeed 		= 400000;
 	I2cHandle.Init.DualAddressMode 	= I2C_DUALADDRESS_DISABLED;
-	I2cHandle.Init.DutyCycle		= I2C_DUTYCYCLE_16_9;
+	I2cHandle.Init.DutyCycle		= I2C_DUTYCYCLE_2;
 	I2cHandle.Init.GeneralCallMode	= I2C_GENERALCALL_DISABLED;
 	I2cHandle.Init.NoStretchMode	= I2C_NOSTRETCH_DISABLED;
-	I2cHandle.Init.OwnAddress1		= CODEC_ADDRESS;
-	I2cHandle.Init.OwnAddress2		= 0x00;
+	I2cHandle.Init.OwnAddress1		= 0x11;
 
-	HAL_I2C_Init(&I2cHandle);
+	initSuccess = HAL_I2C_Init(&I2cHandle);
+
+	return(initSuccess);
 }
 
-void CODEC_i2cDataTransfer(uint8_t *buffer, uint8_t size)
+void CODEC_WriteRegister(uint8_t addr, uint8_t value)
 {
-	while(HAL_I2C_Master_Transmit(&I2cHandle, CODEC_ADDRESS << 1, (uint8_t *)buffer, size, 5000) != HAL_OK);
+	// 7-bit address & first data bit
+	uint8_t initAddr = ((addr << 1) & 0xFE) | ((value >> 8) & 0x01);
+
+	// Ensure device can be communicated with
+	while(HAL_I2C_IsDeviceReady(&I2cHandle, CODEC_I2C_ADDRESS, 1, 1000) != HAL_OK);
+
+	// Write to register in codec format
+	while(HAL_I2C_Mem_Write(&I2cHandle, CODEC_I2C_ADDRESS, initAddr, I2C_MEMADD_SIZE_8BIT, (uint8_t *) value, (uint16_t) 1, 1000) != HAL_OK);
 }
--- src/codec.h
+++ src/codec.h
@@ -20,9 +20,37 @@
 
 #include "stm32f4-hal/stm32f4xx_hal_i2c.h"
 
-#define CODEC_ADDRESS 	0b0011010
+#define CODEC_I2C_ADDRESS 	(0b0011010) << 1
 
-void CODEC_i2c2Init( void );
-void CODEC_i2cDataTransfer(uint8_t *buffer, uint8_t size);
+/* I2C */
+#define CODEC_I2C_GPIO		GPIOB
+#define CODEC_I2C_SCL_PIN	GPIO_PIN_10
+#define CODEC_I2C_SDA_PIN	GPIO_PIN_11
+
+/* I2S */
+#define CODEC_I2S_GPIO		GPIOB
+#define CODEC_I2S_WS_PIN	GPIO_PIN_12
+#define CODEC_I2S_extSD_PIN GPIO_PIN_14
+#define CODEC_I2S_CK_PIN	GPIO_PIN_13
+#define CODEC_I2S_SD_PIN	GPIO_PIN_15
+
+#define CODEC_I2S_MCK_GPIO	GPIOC
+#define CODEC_I2S_MCK_PIN	GPIO_PIN_6
+
+/* Registers */
+#define CODEC_LLINEIN_REG	0x00
+#define CODEC_RLINEIN_REG	0x01
+#define CODEC_LHPOUT_REG	0x02
+#define CODEC_RHPOUT_REG	0x03
+#define CODEC_APATHCTRL_REG	0x04
+#define CODEC_DPATHCTRL_REG	0x05
+#define CODEC_PDOWN_REG		0x06
+#define CODEC_DFMT_REG		0x07
+#define CODEC_SAMPCTRL_REG	0x08
+#define CODEC_ACTIVE_REG	0x09
+#define CODEC_RESET_REG		0x0f
+
+HAL_StatusTypeDef CODEC_i2c2Init(void);
+void CODEC_WriteRegister( uint8_t addr, uint8_t value );
 
 #endif /* CODEC_H_ */
